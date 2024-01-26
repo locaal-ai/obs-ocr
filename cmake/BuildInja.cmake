addb9233a7a2970884bfe25a057770a6f7cfd217
Roy Shilkrot <roy.shil@gmail.com>
2024-01-26 12:34:42 -0500

    Disable shadowing warnings in inja build

--- cmake/BuildInja.cmake
+++ cmake/BuildInja.cmake
@@ -7,4 +7,5 @@ set(BUILD_BENCHMARK OFF)
 set(COVERALLS OFF)
 # inja uses a lot of shadowing
 add_compile_options(-Wno-shadow)
+add_compile_options(-Wno-error=shadow)
 add_subdirectory(${CMAKE_SOURCE_DIR}/vendor/inja ${CMAKE_BINARY_DIR}/inja EXCLUDE_FROM_ALL)
