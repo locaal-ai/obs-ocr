44a07985578328f5a68ea1b7cc25f9198147d2a0
Roy Shilkrot <roy.shil@gmail.com>
2024-01-29 14:19:17 -0500

    Add Fedora build instructions (#8)
    * Add Fedora build instructions
    
    * Update docs/BUILD-FEDORA.md
    
    Co-authored-by: Kaito Udagawa <umireon@gmail.com>
    
    * Update submodule URL in .gitmodules file
    
    * Disable shadowing warnings in inja build
    
    * Disable conversion errors on Linux
    
    * Remove -Wshadow option from gcc compiler configuration
    
    ---------
    
    Co-authored-by: Kaito Udagawa <umireon@gmail.com>

--- cmake/BuildInja.cmake
+++ cmake/BuildInja.cmake
@@ -7,4 +7,11 @@ set(BUILD_BENCHMARK OFF)
 set(COVERALLS OFF)
 # inja uses a lot of shadowing
 add_compile_options(-Wno-shadow)
+add_compile_options(-Wno-error=shadow)
 add_subdirectory(${CMAKE_SOURCE_DIR}/vendor/inja ${CMAKE_BINARY_DIR}/inja EXCLUDE_FROM_ALL)
+
+# on linux, disable conversion errors
+if(UNIX AND NOT APPLE)
+  set(CMAKE_COMPILE_WARNING_AS_ERROR OFF)
+  add_compile_options(-Wno-error -Wno-conversion -Wno-shadow -Wno-unused-parameter)
+endif()
