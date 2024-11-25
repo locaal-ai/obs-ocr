b9eee36c574b0b5ffeb0be7c444c7ecef70e2916
Roy Shilkrot <roy.shil@gmail.com>
2024-11-24 21:51:14 -0500

    Remove unused media event callback functions from OCR filter

--- src/ocr-filter-callbacks.cpp
+++ src/ocr-filter-callbacks.cpp
@@ -4,46 +4,6 @@
 
 #include <obs.h>
 
-void media_play_callback(void *data_, calldata_t *cd)
-{
-	UNUSED_PARAMETER(cd);
-	filter_data *gf_ = static_cast<struct filter_data *>(data_);
-	obs_log(LOG_INFO, "media_play");
-	gf_->isDisabled = false;
-}
-
-void media_started_callback(void *data_, calldata_t *cd)
-{
-	UNUSED_PARAMETER(cd);
-	filter_data *gf_ = static_cast<struct filter_data *>(data_);
-	obs_log(LOG_INFO, "media_started");
-	gf_->isDisabled = false;
-}
-
-void media_pause_callback(void *data_, calldata_t *cd)
-{
-	UNUSED_PARAMETER(cd);
-	filter_data *gf_ = static_cast<struct filter_data *>(data_);
-	obs_log(LOG_INFO, "media_pause");
-	gf_->isDisabled = true;
-}
-
-void media_restart_callback(void *data_, calldata_t *cd)
-{
-	UNUSED_PARAMETER(cd);
-	filter_data *gf_ = static_cast<struct filter_data *>(data_);
-	obs_log(LOG_INFO, "media_restart");
-	gf_->isDisabled = false;
-}
-
-void media_stopped_callback(void *data_, calldata_t *cd)
-{
-	UNUSED_PARAMETER(cd);
-	filter_data *gf_ = static_cast<struct filter_data *>(data_);
-	obs_log(LOG_INFO, "media_stopped");
-	gf_->isDisabled = true;
-}
-
 void enable_callback(void *data_, calldata_t *cd)
 {
 	filter_data *gf_ = static_cast<struct filter_data *>(data_);
--- src/ocr-filter-callbacks.h
+++ src/ocr-filter-callbacks.h
@@ -2,9 +2,4 @@
 
 #include <callback/calldata.h>
 
-void media_play_callback(void *data_, calldata_t *cd);
-void media_started_callback(void *data_, calldata_t *cd);
-void media_pause_callback(void *data_, calldata_t *cd);
-void media_restart_callback(void *data_, calldata_t *cd);
-void media_stopped_callback(void *data_, calldata_t *cd);
 void enable_callback(void *data_, calldata_t *cd);
